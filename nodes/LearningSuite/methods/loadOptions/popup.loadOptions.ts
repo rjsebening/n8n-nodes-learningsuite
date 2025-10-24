fa9ebd363079a40833f7e7f3b7401f85e9e38293
rjsebening <github@rezk-sebening.com>
2025-10-24 20:10:25 +0200

    fix: popup name not showing in dropdown

--- nodes/LearningSuite/methods/loadOptions/popup.loadOptions.ts
+++ nodes/LearningSuite/methods/loadOptions/popup.loadOptions.ts
@@ -2,5 +2,5 @@ import type { ILoadOptionsFunctions } from 'n8n-workflow';
 import { fetchOptions } from './common';
 
 export async function popup_getPopups(this: ILoadOptionsFunctions) {
-	return fetchOptions.call(this, '/popups', undefined, ['name', 'title'], ['id', 'sid']);
+	return fetchOptions.call(this, '/popups', undefined, ['internalName', 'sid'], ['id']);
 }
