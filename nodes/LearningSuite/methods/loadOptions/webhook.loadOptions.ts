8e9be7dd95a2478aa1977c261cf9b5d37127a68b
Rezk Jörg Sebening <github@rezk-sebening.com>
2026-01-12 19:41:24 +0100

    feat(load-options): adjust webhook subscription load options

--- nodes/LearningSuite/methods/loadOptions/webhook.loadOptions.ts
+++ nodes/LearningSuite/methods/loadOptions/webhook.loadOptions.ts
@@ -1,6 +1,18 @@
 import type { ILoadOptionsFunctions } from 'n8n-workflow';
-import { fetchOptions } from './common';
+import { lsRequest } from '../../shared';
+import { ensureArray } from './common';
 
 export async function webhook_getSubscriptions(this: ILoadOptionsFunctions) {
-	return fetchOptions.call(this, '/webhooks/subscription', undefined, ['hookUrl', 'name'], ['id', 'sid']);
+	const res = await lsRequest.call(this, 'GET', '/webhooks/subscription');
+	const rows = ensureArray(res);
+
+	return rows.map((s: any) => {
+		const label = s?.type ?? 'Unknown type';
+
+		return {
+			name: String(label),
+			description: String(s?.url),
+			value: String(s?.id),
+		};
+	});
 }
